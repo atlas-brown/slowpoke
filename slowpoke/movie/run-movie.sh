2a2ad3c99ca9e469897baea84bc3b105f340abf3
Yizheng Xie <yizhengx@bu.edu>
2025-03-22 22:18:30 +0000

    Add movie results

--- slowpoke/movie/run-movie.sh
+++ slowpoke/movie/run-movie.sh
@@ -4,5 +4,5 @@ cd $(dirname $0)/..
 
 # python3 test.py -b movie -r mix -x castinfo --num_exp 5 -t 8 -c 512
 
-python3 test.py -b movie -r mix -x moviereviews --num_exp 10 -t 4 -c 256 >movie/new-results/mix-moviereviews-4-256.log
+python3 test.py -b movie -r mix -x moviereviews --num_exp 10 -t 8 -c 1024 >movie/new-results/mix-moviereviews-8-1024-50000-10-3.log
 
