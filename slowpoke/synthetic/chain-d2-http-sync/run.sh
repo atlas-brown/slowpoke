bb885843ba6bac6029ed6bfaca432ddb6a7c105d
Ubuntu <ubuntu@ip-172-31-44-24.ec2.internal>
2025-03-22 17:13:02 +0000

    Add d2 results

--- slowpoke/synthetic/chain-d2-http-sync/run.sh
+++ slowpoke/synthetic/chain-d2-http-sync/run.sh
@@ -4,24 +4,35 @@ cd $(dirname $0)/../..
 
 exp=chain-d2-http-sync
 
-mkdir -p synthetic/$exp/results
+mkdir -p synthetic/$exp/one-service-per-node-results
 
-target_services="0 1 2"
-for target_service in $target_services
+# Make it reproducible
+target_service_random_pairs="0:4446 1:7748 2:22717"
+# target_service_random_pairs="0:4446"
+
+for pair in $target_service_random_pairs
 do 
-    if [[ -e synthetic/$exp/results/$exp-service$target_service.log ]]; then
-        echo "File synthetic/$exp/results/$exp-service$target_service.log already exists. Skipping..."
+    target_service=$(echo $pair | cut -d':' -f1)
+    random_seed=$(echo $pair | cut -d':' -f2)
+
+    output_file=synthetic/$exp/one-service-per-node-results/$exp-service$target_service.log
+    
+    if [[ -e $output_file ]]; then
+        echo "File $output_file already exists. Skipping..."
         continue
     fi
-    touch synthetic/$exp/results/$exp-service$target_service.log
+
+    touch $output_file
+    
     python3 test.py -b synthetic \
         -r $exp \
         -x service$target_service \
         --num_exp 10 \
-        -c 128 \
-        -t 2 \
-        --num_req 18000 \
+        -c 256 \
+        -t 4 \
+        --num_req 20000 \
         --clien_cpu_quota 2 \
-        --random_seed $RANDOM \
-        >synthetic/$exp/results/$exp-service$target_service.log
+        --random_seed $random_seed \
+        --repetition 3 \
+        >$output_file
 done
