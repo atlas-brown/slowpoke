72e5daa54604f68fd8c068ee82446352eb88d09e
Yizheng Xie <yizhengx@bu.edu>
2025-03-22 05:16:34 +0000

    Add hotel res

--- slowpoke/hotel/run-hotel.sh
+++ slowpoke/hotel/run-hotel.sh
@@ -2,4 +2,4 @@
 
 cd $(dirname $0)/..
 
-python3 test.py -b hotel -r mix -x profile --num_exp 10 -t 8 -c 512 --clien_cpu 8 >hotel/new-results/mix-profile.log
+python3 test.py -b hotel -r mix -x profile --num_exp 10 -t 8 -c 512 --clien_cpu 8 >hotel/new-results/mix-profile-use-wrk-output.log
