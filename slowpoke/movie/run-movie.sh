6c2b1da51786cf143d5cb9fbb4159e13fc11ccf0
Yizheng Xie <yizhengx@bu.edu>
2025-03-22 01:15:13 +0000

    Add movie/social run command

--- slowpoke/movie/run-movie.sh
+++ slowpoke/movie/run-movie.sh
@@ -4,5 +4,5 @@ cd $(dirname $0)/..
 
 # python3 test.py -b movie -r mix -x castinfo --num_exp 5 -t 8 -c 512
 
-python3 test.py -b movie -r mix -x moviereviews --num_exp 10 -t 4 -c 256
+python3 test.py -b movie -r mix -x moviereviews --num_exp 10 -t 4 -c 256 >movie/new-results/mix-moviereviews-4-256.log
 
--- slowpoke/social/run-social.sh
+++ slowpoke/social/run-social.sh
@@ -3,6 +3,6 @@
 cd $(dirname $0)/..
 
 # python3 test.py -b social -r mix -x composepost --num_exp 5
-python3 test.py -b social -r mix -x usertimeline --num_exp 10
+python3 test.py -b social -r mix -x hometimeline --num_exp 10 >social/new-results/hometimeline.log
 # python3 test.py -b social -r mix -x hometimeline --num_exp 10
 
