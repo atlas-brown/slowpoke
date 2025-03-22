3e7c36cccfc8580821a95400d28d9c733bdb090f
Yizheng Xie <yizhengx@bu.edu>
2025-03-22 04:22:35 +0000

    Add social res

--- slowpoke/social/run-social.sh
+++ slowpoke/social/run-social.sh
@@ -3,6 +3,6 @@
 cd $(dirname $0)/..
 
 # python3 test.py -b social -r mix -x composepost --num_exp 5
-python3 test.py -b social -r mix -x hometimeline --num_exp 10 >social/new-results/hometimeline.log
+python3 test.py -b social -r mix -x hometimeline -t 8 -c 512 --num_exp 10 >social/new-results/hometimeline-8-512-100000.log
 # python3 test.py -b social -r mix -x hometimeline --num_exp 10
 
