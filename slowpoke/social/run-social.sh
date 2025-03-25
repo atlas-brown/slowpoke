fef5d33c20b925eb72b013b34b3153dd4e217621
Yizheng Xie <yizhengx@bu.edu>
2025-03-25 22:05:03 +0000

    Add social 60ms batching result

--- slowpoke/social/run-social.sh
+++ slowpoke/social/run-social.sh
@@ -4,10 +4,10 @@ cd $(dirname $0)/..
 
 DIR=social/one-service-per-node-poker-time-spining
 mkdir -p $DIR
-FILE=$DIR/hometimeline-4-128-50000-poker60000000.log
+FILE=$DIR/hometimeline-4-128-50000-poker60000000-full.log
 # Check if exists to avoid overwritting
 if [ -e "$FILE" ]; then
     echo "File $FILE already exists. Exiting..."
     exit 1  # Exit with status code 1 to indicate an error
 fi
-python3 test.py -b social -r mix -x hometimeline -t 4 -c 128 --num_exp 2 --poker_batch 60000000 --num_req 50000 >$FILE
+python3 test.py -b social -r mix -x hometimeline -t 4 -c 128 --num_exp 10 --poker_batch 60000000 --num_req 50000 >$FILE
