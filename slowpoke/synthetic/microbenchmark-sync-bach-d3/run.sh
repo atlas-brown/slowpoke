1ffc6a8b99b575b64868162bd9a14cc40f3ccd4c
Ubuntu <ubuntu@ip-172-31-40-102.ec2.internal>
2025-04-07 02:32:41 +0000

    Add synced batch microbench res

---
+++ slowpoke/synthetic/microbenchmark-sync-bach-d3/run.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+
+cd $(dirname $0)/../..
+
+EXP=chain-d4-http-sync
+DIR=synthetic/$EXP/bus-theory-fixed-json-time-based-sleep
+mkdir -p $DIR
+
+# config
+THREAD=8
+CONN=512
+NUM_REQ=40000
+POKER_BATCH=40000000
+NUM_EXP=10
+REPETITION=1
+
+# Make it reproducible
+target_service_random_pairs="0:31122 2:28561 5:6536"
+# target_service_random_pairs="0:31122"
+
+for pair in $target_service_random_pairs
+do 
+    target_service=$(echo $pair | cut -d':' -f1)
+    random_seed=$(echo $pair | cut -d':' -f2)
+
+    output_file=$DIR/$EXP-service$target_service-t$THREAD-c$CONN-req$NUM_REQ-poker$POKER_BATCH-n$NUM_EXP-rep$REPETITION-largerExecT.log
+    
+    if [[ -e $output_file ]]; then
+        echo "File $output_file already exists. Skipping..."
+        continue
+    fi
+
+    touch $output_file
+    
+    python3 test.py -b synthetic \
+        -r $EXP \
+        -x service$target_service \
+        --num_exp $NUM_EXP \
+        -c $CONN \
+        -t $THREAD \
+        --num_req $NUM_REQ \
+        --random_seed $random_seed \
+        --repetition $REPETITION \
+        --poker_batch $POKER_BATCH \
+        >$output_file
+done
