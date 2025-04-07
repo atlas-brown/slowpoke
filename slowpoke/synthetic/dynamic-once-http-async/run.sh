f2088cb3768c8ddb01326e3fc9c0e22d718355eb
Yizheng Xie <yizhengx@bu.edu>
2025-04-07 04:26:24 +0000

    Add dynamic once http async res

--- slowpoke/synthetic/dynamic-once-http-async/run.sh
+++ slowpoke/synthetic/dynamic-once-http-async/run.sh
@@ -2,26 +2,45 @@
 
 cd $(dirname $0)/../..
 
-exp=dynamic-once-http-async
+EXP=dynamic-once-http-async
+DIR=synthetic/$EXP/bus-theory-fixed-json-time-based-sleep
+mkdir -p $DIR
 
-mkdir -p synthetic/$exp/results
+# config
+THREAD=8
+CONN=512
+NUM_REQ=40000
+POKER_BATCH=20000000
+NUM_EXP=10
+REPETITION=1
 
-target_services="1 2"
-for target_service in $target_services
+# Make it reproducible
+target_service_random_pairs="1:14495 2:14495"
+
+for pair in $target_service_random_pairs
 do 
-    if [[ -e synthetic/$exp/results/$exp-service$target_service.log ]]; then
-        echo "File synthetic/$exp/results/$exp-service$target_service.log already exists. Skipping..."
+    target_service=$(echo $pair | cut -d':' -f1)
+    random_seed=$(echo $pair | cut -d':' -f2)
+
+    output_file=$DIR/$EXP-service$target_service-t$THREAD-c$CONN-req$NUM_REQ-poker$POKER_BATCH-n$NUM_EXP-rep$REPETITION-relative100-new-testpy.log
+    
+    if [[ -e $output_file ]]; then
+        echo "File $output_file already exists. Skipping..."
         continue
     fi
-    touch synthetic/$exp/results/$exp-service$target_service.log
+
+    touch $output_file
+    
     python3 test.py -b synthetic \
-        -r $exp \
+        -r $EXP \
         -x service$target_service \
-        --num_exp 10 \
-        -c 128 \
-        -t 2 \
-        --num_req 18000 \
-        --clien_cpu_quota 2 \
-        --random_seed $RANDOM \
-        >synthetic/$exp/results/$exp-service$target_service.log
+        --num_exp $NUM_EXP \
+        -c $CONN \
+        -t $THREAD \
+        --num_req $NUM_REQ \
+        --random_seed $random_seed \
+        --repetition $REPETITION \
+        --poker_batch $POKER_BATCH \
+        >$output_file
 done
+
