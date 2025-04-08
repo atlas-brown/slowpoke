67adc021d994238f260ee1f7b93ddfe06c4f0a17
Yizheng Xie <yizhengx@bu.edu>
2025-04-08 02:02:01 +0000

    Add hotel res

--- slowpoke/hotel/run-hotel.sh
+++ slowpoke/hotel/run-hotel.sh
@@ -7,14 +7,14 @@ thread=8
 conn=1024
 repetitions=2
 num_req=20000
-poker_batch=40000000
+poker_batch_req=200
 num_exp=10
 DIR=hotel/04-06-all-flush-time-based-sleep
-FILE=mix-$target-t$thread-c$conn-r$repetitions-req$num_req-n$num_exp-batch$poker_batch-relative100reqbatch.log
+FILE=mix-$target-t$thread-c$conn-r$repetitions-req$num_req-n$num_exp-poker_batch_req$poker_batch_req-2.log
 mkdir -p $DIR
 if [ -f $DIR/$FILE ]; then
     echo "File $DIR/$FILE already exists. Skipping test."
     exit 0
 fi
 
-python3 test.py -b hotel -r mix -x profile --num_exp $num_exp -t $thread -c $conn --poker_batch $poker_batch --repetition $repetitions --num_req $num_req >$DIR/$FILE
+python3 test.py -b hotel -r mix -x profile --num_exp $num_exp -t $thread -c $conn --poker_batch_req $poker_batch_req --repetition $repetitions --num_req $num_req >$DIR/$FILE
