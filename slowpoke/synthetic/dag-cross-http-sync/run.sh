a44d6d62ec6e2410a196c6073fa82b12dc4aeceb
Ubuntu <ubuntu@ip-172-31-12-246.ec2.internal>
2025-04-08 05:13:48 +0000

    Add dag-cross-http-sync res

--- slowpoke/synthetic/dag-cross-http-sync/run.sh
+++ slowpoke/synthetic/dag-cross-http-sync/run.sh
@@ -9,14 +9,15 @@ mkdir -p $DIR
 # config
 THREAD=8
 CONN=512
-NUM_REQ=40000
-POKER_BATCH=20000000
+NUM_REQ=20000
+# POKER_BATCH=20000000
+POKER_BATCH_REQ=100
 NUM_EXP=10
 REPETITION=1
 
 # Make it reproducible
-target_service_random_pairs="2:12895 3:19434"
 # target_service_random_pairs="2:12895 3:19434 4:1348"
+target_service_random_pairs="4:1348"
 # 4:1348 has giant processing time
 
 for pair in $target_service_random_pairs
@@ -24,7 +25,7 @@ do
     target_service=$(echo $pair | cut -d':' -f1)
     random_seed=$(echo $pair | cut -d':' -f2)
 
-    output_file=$DIR/$EXP-service$target_service-t$THREAD-c$CONN-req$NUM_REQ-poker$POKER_BATCH-n$NUM_EXP-rep$REPETITION-relative100.log
+    output_file=$DIR/$EXP-service$target_service-t$THREAD-c$CONN-req$NUM_REQ-poker_batch_req$POKER_BATCH_REQ-n$NUM_EXP-rep$REPETITION-relative100.log
     
     if [[ -e $output_file ]]; then
         echo "File $output_file already exists. Skipping..."
@@ -42,7 +43,7 @@ do
         --num_req $NUM_REQ \
         --random_seed $random_seed \
         --repetition $REPETITION \
-        --poker_batch $POKER_BATCH \
+        --poker_batch_req $POKER_BATCH_REQ \
         >$output_file
 done
 
