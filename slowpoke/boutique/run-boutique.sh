3faa2b5ed59f67e572800761d35bb988b1c7b8c1
Ubuntu <ubuntu@ip-172-31-12-246.ec2.internal>
2025-04-08 22:38:22 +0000

    Add boutique res

--- slowpoke/boutique/run-boutique.sh
+++ slowpoke/boutique/run-boutique.sh
@@ -9,13 +9,13 @@ thread=8
 conn=512
 repetitions=3
 num_req=200000
-poker_batch=30000000
+poker_batch_req=100
 num_exp=10
-DIR=boutique/04-06-all-flush-time-based-sleep
-FILE=mix-$target-t$thread-c$conn-r$repetitions-req$num_req-n$num_exp-batch$poker_batch-all-flush-relative100reqbatch.log
+DIR=boutique/04-08-pokerpp
+FILE=mix-$target-t$thread-c$conn-r$repetitions-req$num_req-n$num_exp-poker_batch_req$poker_batch_req.log
 mkdir -p $DIR
 if [ -f $DIR/$FILE ]; then
     echo "File $DIR/$FILE already exists. Skipping test."
     exit 0
 fi
-python3 test.py -b boutique -x cart -r mix -t $thread -c $conn --num_exp $num_exp --repetitions $repetitions --num_req $num_req --poker_batch $poker_batch >$DIR/$FILE
+python3 test.py -b boutique -x cart -r mix -t $thread -c $conn --num_exp $num_exp --repetitions $repetitions --num_req $num_req --poker_batch_req $poker_batch_req >$DIR/$FILE
