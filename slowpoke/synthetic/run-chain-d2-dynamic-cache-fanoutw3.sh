9e4240cd0cc070ec5ce977ea9fccf3fff1c8408d
Ubuntu <ubuntu@ip-172-31-40-102.ec2.internal>
2025-04-10 18:24:22 +0000

    Only porting to pokerpp

---
+++ slowpoke/synthetic/run-chain-d2-dynamic-cache-fanoutw3.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+cd $(dirname $0)
+
+all_prefix="chain-d2 dynamic-cache fanout-w3-http-async"
+
+for prefix in $all_prefix
+do
+    for folder in ./$prefix*
+    do
+    kubectl get deploy -o name | xargs kubectl delete
+    kubectl get svc -o name | xargs kubectl delete 
+    bash $folder/run.sh
+    # clean up all deployment and services
+    done
+done
