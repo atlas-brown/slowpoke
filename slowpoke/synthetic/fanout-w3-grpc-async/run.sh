1e2848f7534327f969e7685fcabed4ec7c75c40c
Ubuntu <ubuntu@ip-172-31-12-246.ec2.internal>
2025-04-12 16:52:18 +0000

    port fanout-w3-grpc-async

--- slowpoke/synthetic/fanout-w3-grpc-async/run.sh
+++ slowpoke/synthetic/fanout-w3-grpc-async/run.sh
@@ -1,20 +1,28 @@
 #!/bin/bash
 
+# Change this!!
+target_service_random_pairs="0:21032 2:18674" # Make it reproducible
+
 cd $(dirname $0)/../..
 
-exp=fanout-w3-grpc-async
+EXP=$(dirname $0 | xargs basename)
+DIR=synthetic/$EXP/04-09-pokerpp-rm-deadlock-0maxconn
+mkdir -p $DIR
 
-mkdir -p synthetic/$exp/one-service-per-node-results
-
-# Make it reproducible
-target_service_random_pairs="0:3047 2:7970"
+# config
+THREAD=8
+CONN=800
+NUM_REQ=20000
+POKER_BATCH_REQ=100
+NUM_EXP=10
+REPETITION=3
 
 for pair in $target_service_random_pairs
 do 
     target_service=$(echo $pair | cut -d':' -f1)
     random_seed=$(echo $pair | cut -d':' -f2)
 
-    output_file=synthetic/$exp/one-service-per-node-results/$exp-service$target_service-8-512-10-3.log
+    output_file=$DIR/$EXP-service$target_service-t$THREAD-c$CONN-req$NUM_REQ-poker_batch_req$POKER_BATCH_REQ-n$NUM_EXP-rep$REPETITION.log
     
     if [[ -e $output_file ]]; then
         echo "File $output_file already exists. Skipping..."
@@ -24,13 +32,14 @@ do
     touch $output_file
     
     python3 test.py -b synthetic \
-        -r $exp \
+        -r $EXP \
         -x service$target_service \
-        --num_exp 10 \
-        -c 512 \
-        -t 8 \
-        --num_req 20000 \
+        --num_exp $NUM_EXP \
+        -c $CONN \
+        -t $THREAD \
+        --num_req $NUM_REQ \
         --random_seed $random_seed \
-        --repetition 3 \
+        --repetition $REPETITION \
+        --poker_batch_req $POKER_BATCH_REQ \
         >$output_file
 done
